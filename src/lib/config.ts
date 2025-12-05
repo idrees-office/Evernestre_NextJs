bb75a41229b654478057ea3646c57994b9b792fc
idrees-office <idreesoffice9012@gmail.com>
2025-12-05 16:18:46 +0400

    update codes

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,4 +1,5 @@
-export const BASE_URL = "http://127.0.0.1:8000/api/website";
+// export const BASE_URL = "http://127.0.0.1:8000/api/website";
 // export const BASE_URL = "https://backend.evernest.ae/api/website";
+export const BASE_URL = "https://backend.leadshub.ae/api/website";
 // export const BASE_URL = "http://newcrmbackend.ddev.site/api/website";
 export const IMAGE_BASE_URL = process.env.NEXT_PUBLIC_IMAGE_API_URL || "http://localhost:8000";
