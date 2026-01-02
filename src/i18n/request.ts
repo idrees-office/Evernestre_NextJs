de312fa5a8cf6b75882583cc2681481f02002fad
idrees-office <idreesoffice9012@gmail.com>
2026-01-02 18:08:57 +0400

    update codes

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,34 +1,55 @@
-// // import { getRequestConfig } from 'next-intl/server';
+// import { getRequestConfig } from 'next-intl/server';
+// export default getRequestConfig(async ({ requestLocale }) => {
+//   let locale = await requestLocale;
+//   if (!locale) {
+//     locale = 'en';
+//   }
+//   const normalizedLocale = locale.split('-')[0];
+//   try {
+//     let messages;
+//     if (normalizedLocale === 'cz') {
+//       messages = (await import(`../messages/cz.json`)).default;
+//     } else if(normalizedLocale === 'ru') {
+//       messages = (await import(`../messages/ru.json`)).default;
+//     } else if(normalizedLocale === 'es') {
+//       messages = (await import(`../messages/es.json`)).default;
+//     }
+//     else {
+//       messages = (await import(`../messages/en.json`)).default;
+//     }
 
-// // export default getRequestConfig(async ({ locale }) => {
-// //   console.log('🔥 locale received in request.ts =', locale);
+//     return {
+//       locale: normalizedLocale,
+//       messages
+//     };
+//   } catch (error) {
+//     const fallbackMessages = (await import(`../messages/en.json`)).default;
+//     return {
+//       locale: 'en',
+//       messages: fallbackMessages
+//     };
+//   }
+// });
 
-// //   const resolvedLocale = locale ?? 'en';
 
-// //   return {
-// //     locale: resolvedLocale,
-// //     messages: (await import(`../messages/${resolvedLocale}.json`)).default
-// //   };
-// // });
 
 import { getRequestConfig } from 'next-intl/server';
-export default getRequestConfig(async ({ requestLocale }) => {
-  let locale = await requestLocale;
-  if (!locale) {
-    locale = 'en';
-  }
-  const normalizedLocale = locale.split('-')[0];
+
+export default getRequestConfig(async ({ locale }) => {
+  const resolvedLocale = locale ?? 'en';
+  const normalizedLocale = resolvedLocale.split('-')[0];
+
   try {
     let messages;
+
     if (normalizedLocale === 'cz') {
-      messages = (await import(`../messages/cz.json`)).default;
-    } else if(normalizedLocale === 'ru') {
-      messages = (await import(`../messages/ru.json`)).default;
-    } else if(normalizedLocale === 'es') {
-      messages = (await import(`../messages/es.json`)).default;
-    }
-    else {
-      messages = (await import(`../messages/en.json`)).default;
+      messages = (await import('../messages/cz.json')).default;
+    } else if (normalizedLocale === 'ru') {
+      messages = (await import('../messages/ru.json')).default;
+    } else if (normalizedLocale === 'es') {
+      messages = (await import('../messages/es.json')).default;
+    } else {
+      messages = (await import('../messages/en.json')).default;
     }
 
     return {
@@ -36,12 +57,12 @@ export default getRequestConfig(async ({ requestLocale }) => {
       messages
     };
   } catch (error) {
-    const fallbackMessages = (await import(`../messages/en.json`)).default;
     return {
       locale: 'en',
-      messages: fallbackMessages
+      messages: (await import('../messages/en.json')).default
     };
   }
 });
+
 
 
